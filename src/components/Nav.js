5e016f23fe53e7e419566002e2d1f3b1986b6f32
Leah Sandacz <sandas@outlook.com>
2018-05-15 19:06:42 -0700

    Nav menu

---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const NavLinkStyle = {
+  color: 'black',
+  margin: '0 0.5rem',
+};
+
+const Nav = () => (
+  <div
+    style={{
+      background: 'AliceBlue',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1rem 1rem',
+      }}
+    >
+      <Link to='/' style={NavLinkStyle}>Home</Link>
+      <Link to='/games' style={NavLinkStyle}>Games</Link>
+    </div>
+  </div>
+)
+
+
+export default Nav
