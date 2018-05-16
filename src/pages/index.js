feead262217edcb608569ba9560ae4fd69a20e96
Leah Sandacz <sandas@outlook.com>
2018-05-15 17:44:37 -0700

    Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Welcome</h1>
+    <p>Welcome to Code Inferno Games! I'm a game developer (who sometimes works with artists!) with an interest in building challenging casual games.</p>
+    <p>Currently, no games are being actively developed, but that may change in the future.</p>
+    <p>This site includes an archive of all games released under this moniker, <Link to="/games/">so check them out!</Link></p>
   </div>
 )
 
